56d499d876400a477fc7cce1ee68586177356b82
Josh <jmal11six@gmail.com>
2025-07-12 21:04:45 -0500

    even more bans

--- bot.py
+++ bot.py
@@ -337,7 +337,7 @@ class Bot:
         if 'boostprox.online' in message.message.replace(' ', ''): return True
         try:
             is_unicode = not all(ord(c) < 128 for c in message.message)
-            return ('.com' in message.message or '.ru' in message.message) and is_unicode
+            return ('.com' in message.message or '.ru' in message.message or '.net' in message.message) and is_unicode
         except Exception as e:
             self.log_error_message(f'{e.__class__} deciding to ban a user, you should look at that')
     
